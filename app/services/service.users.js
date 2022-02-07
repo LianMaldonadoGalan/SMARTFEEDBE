5927a24eafd565fc7647723f9e8f493c7745e522
Luis Lopez <lglvluislopez@gmail.com>
2022-02-06 23:46:32 -0600

    feat: logging added

--- app/services/service.users.js
+++ app/services/service.users.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-undef */
 import knexfile from "../knexfile";
+import Pino from 'pino'
+
+const logger = Pino()
 
 const pg = knexfile
 export async function getUser(data) {
@@ -7,7 +10,7 @@ export async function getUser(data) {
     try {
         response = await pg.select('id_user', 'email', 'is_administrator').from('users').where({email: data.email, passwd: data.passwd});
     } catch (error) {
-        console.log(error);
+        logger.error(error);
         response = { error: 'unable to get user' };
     }
     return response;
@@ -19,7 +22,7 @@ export async function insertUser(data) {
     try {
         response = isAdmin ? await pg.returning(['id_user', 'email', 'created_at', 'is_administrator']).insert({ email, passwd, isAdmin }).into('users') : await pg.returning(['id_user', 'email', 'created_at', 'is_administrator']).insert({ email, passwd }).into('users')
     } catch (error) {
-        console.log(error)
+        logger.error()
         response = { error: 'unable to insert user' }
     }
     return response
@@ -32,7 +35,7 @@ export async function updateUser(data) {
     try {
         response = isAdmin ? await pg("users").returning(['id_user', 'email', 'is_administrator']).where({ id_user }).update({ email, passwd, isAdmin, updated_at }) : await pg("users").returning(['id_user', 'email', 'is_administrator']).where({ id_user }).update({ email, passwd, updated_at })
     } catch (error) {
-        console.log(error)
+        logger.error(error);
         response = { error: 'unable to update user' }
     }
     return response
@@ -45,12 +48,13 @@ export async function deleteUser(data) {
         response = await pg("users").returning(['id_user', 'email']).where({ id_user }).del()
     }
     catch (error) {
-        console.log(error)
+        logger.error(error);
         response = { error: 'unable to delete user' }
     }
     return response
 }
 
 export async function terminateConnection() {
+    logger.info('closing connection')
     await pg.destroy();
 }
