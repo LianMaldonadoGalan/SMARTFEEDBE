c2bff36bc443c526a7e11f8b6ab43accd1b727b6
Luis Lopez <lglvluislopez@gmail.com>
2022-04-29 12:52:53 -0500

    refactor: create menu only selects one food

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use("/users", routerUsers);
 app.use("/ingredients", verifyToken, routerIngredients)
 app.use("/meals", verifyToken,routerMeals)
